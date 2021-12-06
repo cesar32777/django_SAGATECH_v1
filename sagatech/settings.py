83760f4662e54555b299f843a30522af91ffb5b4
CÉSAR MUÑIZ CEPEDA <cesar.munizcp@uanl.edu.mx>
2021-12-05 21:34:29 -0600

    Debug set to false

--- sagatech/settings.py
+++ sagatech/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+6@^k(#$ap0n-bln+we(q@p@(5h+o$5s)*-n4wd$$6slf(@7ps'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['143.198.104.21']
 
