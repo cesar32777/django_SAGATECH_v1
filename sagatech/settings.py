711ce389013f3ad112e1037c311020c0bca1c9c6
CÉSAR MUÑIZ CEPEDA <cesar.munizcp@uanl.edu.mx>
2021-12-06 03:11:40 -0600

    mediaroot

--- sagatech/settings.py
+++ sagatech/settings.py
@@ -140,15 +140,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 #STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #STATICFILES_DIRS = [
 #    'sagatech/static',
 #]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
+
+
+#MEDIA_ROOT = BASE_DIR /'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
